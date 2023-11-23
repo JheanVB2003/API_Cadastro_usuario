a4371a14f8f6464e76fde46a00fe7b791661c256
jhean <jheanbarbosa1233@gmail.com>
2023-11-23 01:49:44 -0300

    Método GET buscando pelo ID. Método PUT atualizando com ID. Metodo DELETE ALL adicionado para fins educacional.

--- src/main/java/br/com/cadastro/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/cadastro/usuario/controller/UsuarioController.java
@@ -2,12 +2,16 @@ package br.com.cadastro.usuario.controller;
 
 import br.com.cadastro.usuario.model.Usuario;
 import br.com.cadastro.usuario.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -20,28 +24,40 @@ public class UsuarioController {
         return (List<Usuario>) repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> buscarPeloID(@PathVariable(value = "id")UUID id){
+        Optional<Usuario> usuario = repository.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuario.get());
+    }
+
     @PostMapping
-    public  ResponseEntity<?> criarUsuario(@RequestBody Usuario usuarios){
-
-        if (usuarios.getNome() == null || usuarios.getNome().isEmpty()) {
-            return ResponseEntity.badRequest().body("O campo 'nome' é obrigatório.");
-        }
-
+    public  ResponseEntity<?> criarUsuario(@Valid @RequestBody Usuario usuarios){
         Usuario usuarioNovo  = repository.save(usuarios);
         return ResponseEntity.ok(usuarioNovo);
     }
 
-    @PutMapping
-    public Usuario editarUsuario(@RequestBody Usuario usuarios){
-        Usuario usuarioNovo = repository.save(usuarios);
-        return usuarioNovo;
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> editarUsuario (@PathVariable(value = "id") UUID id,@RequestBody Usuario usuario){
+        Optional<Usuario> usuarioNovo = repository.findById(id);
+        Usuario usuarioExiste = usuarioNovo.get();
+        usuarioExiste.setNome(usuario.getNome());
+        usuarioExiste.setEmail(usuario.getEmail());
+        usuarioExiste.setSenha(usuario.getSenha());
+        usuarioExiste.setTelefone(usuario.getTelefone());
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuarioExiste));
     }
 
     @DeleteMapping("/{id}")
-    public Optional<Usuario> excluirUsuario (@PathVariable Integer id){
+    public ResponseEntity<Object> excluirUsuario (@PathVariable(value = "id") UUID id){
         Optional<Usuario> usuario = repository.findById(id);
         repository.deleteById(id);
-        return usuario;
+        return ResponseEntity.status(HttpStatus.OK).body("Usuário deletado com sucesso");
+    }
+    @DeleteMapping({"","/"})
+    public ResponseEntity<Object> deletaUsuarios(){
+        Iterable<Usuario> usuarios = repository.findAll();
+        repository.deleteAll();
+        return ResponseEntity.status(HttpStatus.OK).body("Todos os usuários foram deletados!");
     }
 
 
--- src/main/java/br/com/cadastro/usuario/repository/UsuarioRepository.java
+++ src/main/java/br/com/cadastro/usuario/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@ package br.com.cadastro.usuario.repository;
 import br.com.cadastro.usuario.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+import java.util.UUID;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, UUID> {
 
 }
